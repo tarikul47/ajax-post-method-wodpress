a62966928cae8aeeac7cad68f1d3be780f5c0b81
Tarikul Islam <tarikul47@gmail.com>
2021-11-05 02:36:03 +0600

    update

--- custom.js
+++ custom.js
@@ -1,5 +1,7 @@
-;(function($){
-    $(document).ready(function(){
+; (function ($) {
+    $(document).ready(function () {
+
+        /*
         $("#send-message").on('click',function(){
 
             $.post(frontend_ajax_object.ajaxurl,{
@@ -15,6 +17,25 @@
 
             console.log('Clicked');
             return false;
+        })
+        */
+
+        $("#mealForm").submit(function (event) {
+            event.preventDefault();
+            const data = {
+                action: 'contact',
+                cn: $('#contact').val(),
+                name: $('#cname').val(),
+                email: $('#cemail').val(),
+                phone: $('#cphone').val(),
+                message: $('#cmessage').val(),
+            };
+
+            $.post(frontend_ajax_object.ajaxurl,data, function(response){
+                console.log(response);
+            })
+            //console.log(data);
+
         })
     });
 })(jQuery);
